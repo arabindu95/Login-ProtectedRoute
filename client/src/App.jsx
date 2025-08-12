439adea2b9d8fb99036e00dfb5dac29b23504602
Arabindu <arabinduchakraborty.921@gmail.com>
2025-08-12 21:03:33 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
